5359521441fb49da2df24dd4a6c9eb9a35f97d29
Lucas Brahm <lucasb@digistar.com.br>
2022-01-28 11:26:55 -0300

    Support Itau boleto creation

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyboletox',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
+    package_data={
+        'pyboletox': ['logos/*.png']
+    }
 )
